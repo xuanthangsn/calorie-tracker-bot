0fdc4dcce0a9a03aea46e0dd6baacbe219714687
txthang <xuanthangsn@gmail.com>
2026-05-04 07:33:37 +0700

    untrack memory folder and test agent file

--- test_agent.py
+++
@@ -1,7 +0,0 @@
-from agent.task import Task
-
-
-# task = Task("for lunch today, I have ate 200gr of grilled chicken breast, 100gr of rice, and 100gr of cabbage")
-task = Task("it would be great if I can buy a new phone this year")
-result = task.execute()
-print(result)
